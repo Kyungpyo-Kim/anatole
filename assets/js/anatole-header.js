34c87611d6ce9195595c91d76a615b60e59e036c
kyungpyo-kim <kyungpyo94@gmail.com>
2022-07-30 20:02:21 +0900

    feat: add taxonomy cloud

--- assets/js/anatole-header.js
+++ assets/js/anatole-header.js
@@ -1,3 +1,16 @@
+function shuffle(items)
+{
+    var cached = items.slice(0), temp, i = cached.length, rand;
+    while(--i)
+    {
+        rand = Math.floor(i * Math.random());
+        temp = cached[rand];
+        cached[rand] = cached[i];
+        cached[i] = temp;
+    }
+    return cached;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   const nav = document.querySelector('nav');
@@ -8,4 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
       nav.classList.toggle('nav--active');
     });
   });
+  
+  var list = document.getElementById(
+      "taxonomy_cloud-contents-ul"
+  );
+  var nodes = list.children, i = 0;
+  nodes = Array.prototype.slice.call(nodes);
+  nodes = shuffle(nodes);
+  while(i < nodes.length)
+  {
+      list.appendChild(nodes[i]);
+      ++i;
+  }
 });
