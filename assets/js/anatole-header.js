d4e273ce11acf04e962ef891a416904b4335450c
Kyungpyo-Kim <Kyungpyo-Kim@users.noreply.github.com>
2022-07-30 11:03:14 +0000

    style: prettier format all files

--- assets/js/anatole-header.js
+++ assets/js/anatole-header.js
@@ -1,14 +1,15 @@
-function shuffle(items)
-{
-    var cached = items.slice(0), temp, i = cached.length, rand;
-    while(--i)
-    {
-        rand = Math.floor(i * Math.random());
-        temp = cached[rand];
-        cached[rand] = cached[i];
-        cached[i] = temp;
-    }
-    return cached;
+function shuffle(items) {
+  var cached = items.slice(0),
+    temp,
+    i = cached.length,
+    rand;
+  while (--i) {
+    rand = Math.floor(i * Math.random());
+    temp = cached[rand];
+    cached[rand] = cached[i];
+    cached[i] = temp;
+  }
+  return cached;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,16 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
       nav.classList.toggle('nav--active');
     });
   });
-  
-  var list = document.getElementById(
-      "taxonomy_cloud-contents-ul"
-  );
-  var nodes = list.children, i = 0;
+
+  var list = document.getElementById('taxonomy_cloud-contents-ul');
+  var nodes = list.children,
+    i = 0;
   nodes = Array.prototype.slice.call(nodes);
   nodes = shuffle(nodes);
-  while(i < nodes.length)
-  {
-      list.appendChild(nodes[i]);
-      ++i;
+  while (i < nodes.length) {
+    list.appendChild(nodes[i]);
+    ++i;
   }
 });
